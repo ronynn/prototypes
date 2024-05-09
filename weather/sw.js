794fb7a570f005f7ac9b6856318ccd6fef3d9c8d
hobbyist <82273354+ronynn@users.noreply.github.com>
2024-05-10 02:23:43 +0530

    mdn guide for pwa 3

--- weather/sw.js
+++ weather/sw.js
@@ -1,17 +1,20 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('weather').then((cache) => cache.addAll([
-      '/prototypes/weather/',
-      '/prototypes/weather/index.html',
-      '/prototypes/weather/script.js',
-     
-    ])),
-  );
+self.addEventListener("install", e => {
+    e.waitUntil(
+        caches
+            .open("weather")
+            .then(cache =>
+                cache.addAll([
+                    "/prototypes/weather/",
+                    "/prototypes/weather/index.html",
+                    "/prototypes/weather/script.js",
+                ])
+            )
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
+self.addEventListener("fetch", e => {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(response => response || fetch(e.request))
+    );
 });
